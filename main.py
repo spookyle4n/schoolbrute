a5707a03949ad9dfd7dca65d3a14a6980420c635
spookyle4n <103913003+spookyle4n@users.noreply.github.com>
2022-11-15 14:14:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ print("attempting to log into schoology with...\033 " + e)
 time.sleep(4)
 print("\033[34msuccessfully logged in as " + e + " [\u2713]")
 
-c = input("\033[30m[?] Victims schoology email?: ")
+c = input("\033[37m[?] Victims schoology email?: ")
 b = int(input("[?] How many passwords would you like to try?: "))
 a = input("[?] Start Attack? [y/n]: ")
 if a == 'y':
@@ -39,7 +39,7 @@ if a == 'y':
         randomUpperLetter = chr(random.randint(ord('A'), ord('Z')))
         randomLowerLetter = chr(random.randint(ord('a'), ord('z')))
         if randomnumber + randomnumber2 + randomnumber3 + randomnumber4 + randomnumber5 + randomnumber6 + randomUpperLetter + randomLowerLetter == '676848Vr':
-            print("\033[32m| attempting... \033[1;3m676848Vr\033[0m |\n\tsuccess! [\u2713] ")
+            print("\033[32m\n\tsuccess! [\u2713] ")
             time.sleep(100000000)
             
         print("\033[31m[+] attempting... " + randomnumber + randomnumber2 + randomnumber3 + randomnumber4 + randomnumber5 + randomnumber6 + randomUpperLetter + randomLowerLetter, flush=True)
