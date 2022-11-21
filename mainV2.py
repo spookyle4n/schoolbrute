196d1490780b435ea1dbb07af313598e495218b4
spookyle4n <103913003+spookyle4n@users.noreply.github.com>
2022-11-21 14:18:38 -0500

    Update mainV2.py

--- mainV2.py
+++ mainV2.py
@@ -13,11 +13,11 @@ print('''
                \033[36m
                
 ''')
-randomnumber7 = chr(random.randint(ord('2'), ord('9')))
+num = random.randrange(3, 9)
 e = input("[?] Your schoology email: ")
 pwd = maskpass.askpass("[?] Your schoology password: ", mask="") 
 print("[+] Attempting to log into schoology with... " + e)
-time.sleep(randomnumber7)
+time.sleep(num)
 print("\033[32m\t> Successfully logged in as " + e + " [\u2713]")
 
 c = input("\033[36m[?] Victims schoology email: ")
