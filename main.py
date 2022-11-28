94c3752389532750b6d89ced063a049f8e8a1da7
spookyle4n <103913003+spookyle4n@users.noreply.github.com>
2022-11-28 09:57:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -35,10 +35,10 @@ if a == 'y':
         randomUpperLetter = chr(random.randint(ord('A'), ord('Z')))
         randomLowerLetter = chr(random.randint(ord('a'), ord('z')))
         if randomnumber + randomnumber2 + randomnumber3 + randomnumber4 + randomnumber5 + randomnumber6 + randomUpperLetter + randomLowerLetter == '676848Vr':
-            print("\033[32m[+] attempting... 676848Vr\nsuccess! [\u2713] ")
+            print("\033[32m[+] password 676848Vr\nsuccess! [\u2713] ")
             print("schoology email: " + c)
             print("schoology password: 676848Vr")
             time.sleep(100000000)
             
-        print("\033[31m[+] attempting... " + randomnumber + randomnumber2 + randomnumber3 + randomnumber4 + randomnumber5 + randomnumber6 + randomUpperLetter + randomLowerLetter, flush=True)
+        print("\033[31m[+] bruting: " + randomnumber + randomnumber2 + randomnumber3 + randomnumber4 + randomnumber5 + randomnumber6 + randomUpperLetter + randomLowerLetter, flush=True)
         time.sleep(0.0000001)
